1d28733ba8ac5b2fc7f35006205f91f070cba974
norihito <norihito0626@gmail.com>
2020-11-26 18:40:00 +0900

    first commit

---
+++ .Rproj.user/ACC643AE/sources/s-b364ac4c/EEF69D74-contents
@@ -0,0 +1,28 @@
+#########################################
+# file  : T2_1d.R
+# auther: norih
+# date  : Nov 26th, 2020
+# Hotelling's T-squared distribution
+# One Dimension
+#########################################
+library(car)
+library(tidyverse)
+data(Davis)
+
+Davis
+
+Davis %>% select(weight)
+
+ggplot(Davis, aes(x=weight)) + geom_histogram()
+
+mu <- mean(Davis$weight)
+s2 <- mean((Davis$weight - mu)^2)
+c(mu, s2)
+
+
+# 異常度の計算
+a <- (Davis$weight - mu)^2 / s2
+th <- qchisq(.99, 1)
+ggplot(NULL, aes(y=a, x = seq(1,length(a)))) + 
+      geom_point(alpha = 1/5) + 
+      geom_hline(yintercept = th, colour = "red", linetype = "dashed")
---
+++ MT.R
@@ -0,0 +1,29 @@
+#########################################
+# file  : MT.R
+# auther: norih
+# date  : Nov 26th, 2020
+# Maharanobis-Taguchi System
+#########################################
+library(MASS)
+library(tidyverse)
+road
+rownames(road)
+
+ggplot(road, aes(y=rownames(road), x=deaths)) + 
+  geom_bar(stat = "identity")
+
+# Davis %>% select(weight)
+# 
+# ggplot(Davis, aes(x=weight)) + geom_histogram()
+# 
+# mu <- mean(Davis$weight)
+# s2 <- mean((Davis$weight - mu)^2)
+# c(mu, s2)
+# 
+# 
+# # 異常度の計算
+# a <- (Davis$weight - mu)^2 / s2
+# th <- qchisq(.99, 1)
+# ggplot(NULL, aes(y=a, x = seq(1,length(a)))) + 
+#       geom_point(alpha = 1/5) + 
+#       geom_hline(yintercept = th, colour = "red", linetype = "dashed")
