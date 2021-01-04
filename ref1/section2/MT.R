ab703a299ecf3e29ad057250ad1ef10a5ff8b2eb
norihito <norihito0626@gmail.com>
2021-01-04 23:14:25 +0900

    revised ref1/section2 png

--- ref1/section2/MT.R
+++ ref1/section2/MT.R
@@ -6,11 +6,10 @@
 #########################################
 library(MASS)
 library(tidyverse)
-road
-rownames(road)
 
-ggplot(road, aes(y=rownames(road), x=deaths)) + 
+p <- ggplot(road, aes(y=rownames(road), x=deaths)) + 
   geom_bar(stat = "identity")
+print(p)
 
 X <- road / road$drivers
 X <- as.matrix(log(X[,-2] + 1))
@@ -18,13 +17,18 @@ mx <- colMeans(X)
 Xc <- X - matrix(1, nrow(X), 1) %*% mx
 Sx <- t(Xc) %*% Xc / nrow(X)
 a <- rowSums((Xc %*% solve(Sx)) * Xc) / ncol(X)
-a
-ggplot(NULL, aes(y=a, x = seq(1,length(a)))) +
+p1 <- ggplot(NULL, aes(y=a, x = seq(1,length(a)))) +
       geom_point(alpha = 1/5) +
       geom_hline(yintercept = 1, colour = "red", linetype = "dashed")
 
 xc_prime <- Xc["Calif",]
 SN1 <- 10*log10(xc_prime^2/diag(Sx))
-ggplot(NULL, aes(x=reorder(names(SN1), SN1), y=SN1, fill = SN1)) + 
+
+p2 <- ggplot(NULL, aes(x=reorder(names(SN1), SN1), y=SN1, fill = SN1)) + 
   geom_bar(stat = "identity")
 
+#install.packages("gridExtra")
+library(gridExtra)
+g <- gridExtra::grid.arrange(p1, p2, nrow = 1)
+
+ggsave("./ref1/section2/fig2.6.png", plot = g)
