3ea8f7032f16970cffe8d0b63cb40770984ffd06
norihito <norihito0626@gmail.com>
2021-01-05 01:53:06 +0900

    gamma function

--- ref1/section3/gamma_function.R
+++ ref1/section3/gamma_function.R
@@ -8,36 +8,33 @@
 
 library(car)
 library(tidyverse)
+library(MASS)
 data(Davis)
 
 N <- length(Davis$weight)
 mu <- mean(Davis$weight)
 si <- sd(Davis$weight) * (N-1)/N #標準偏差 sdは不偏標準偏差
+
 # モーメント法によるGamma関数のkmo, smoを求める。
 kmo <- mu^2 / si^2
 smo <- si^2 / mu
 
 # 最尤推定によるGamma関数のk, s値
+# MASSパッケージのfitdistr 
 ml <- fitdistr(Davis$weight, "gamma")
-kml <- ml$estimate["shape"]
-sml <- 1/ml$estimate["rate"]
+kml <- ml$estimate["shape"] #shape
+sml <- 1/ml$estimate["rate"] #scale
+
+p1 <- ggplot(Davis, aes(x = weight, after_stat(density))) +
+   geom_histogram(color="black", fill="white") +
+   geom_line(aes(x=weight, y=dgamma(weight, shape=kmo, scale=smo)), size = .5) + 
+   geom_line(aes(x=weight, y=dgamma(weight, shape=kml, scale=sml)), size = .5, linetype="dashed")
 
 a <- Davis$weight / smo - (kmo - 1) * log(Davis$weight / smo)
 
 th <- order(a, decreasing = T)[0.01*N]
 
-ggplot(NULL, aes(y=a, seq(1, length(a)))) + 
+p2 <- ggplot(NULL, aes(y=a, seq(1, length(a)))) + 
   geom_point(alpha=0.4) + 
   geom_hline(yintercept = a[th], linetype = "dashed")
-
-# ggplot(Davis, aes(x = weight, after_stat(density))) + 
-#   geom_histogram() +
-#   geom_line(aes(x=Davis$weight, y=dgamma(Davis$weight, kmo, smo)), color="red", size = 1) 
-# 
-# ggplot(NULL, aes(x=seq(40, 200), y=dgamma(seq(40, 200), kmo, smo))) + 
-#   geom_point()
-# 
-# seq(40,200)
-ggplot(NULL, aes(x = seq(0,20, by = 0.2), y = dgamma(seq(0,20, by = 0.2), kmo, smo))) + 
-  geom_line()
 
